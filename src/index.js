022a547d50b5e648199213af99a326b5efad1f11
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-10-09 11:26:21 -0700

    Added CSS support to webpack config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import './styles.css';
 
 const component = () => {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('test');
 
   return element;
 }
