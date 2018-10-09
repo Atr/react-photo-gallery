0c6953a553deab447d56c0f045206fead873b25d
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-10-09 14:03:30 -0700

    Added unique hash to bundle per spec

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     })
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].bundle.[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
