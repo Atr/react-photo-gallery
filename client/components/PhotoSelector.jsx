f1da37a023328a5d70fbdb1479d5e2fb9ba83856
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-10-10 16:21:47 -0700

    Very basic - app resizes based on viewport

--- client/components/PhotoSelector.jsx
+++ client/components/PhotoSelector.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const PhotoSelector = props => (
-  <div className='photo-selector fc fd-c'>
+  <div className='photo-selector fc fd-c ai-c'>
     <div className='caption'>
       Caption goes here
     </div>
-    <div className='center-photo-selector fc'>
-      <button type='button' className='left-button'>
+    <div className='center-photo-selector fc ai-c'>
+      <button type='button' className='left-selector-button'>
         Left arrow
       </button>
-      <img src={props.pic} />
-      <button type='button' className='right-button'>
+      <div className='center-image-container fc jc-c ai-fs'>
+        <img src={props.pic} className='center-image'/>
+      </div>
+      <button type='button' className='right-selector-button'>
         Right arrow
       </button>
     </div>
