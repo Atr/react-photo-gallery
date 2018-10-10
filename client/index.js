70ab6b06c2384927f42c2c5760dd3fdab10eaccd
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-10-10 12:18:28 -0700

    Hello World rendering in React

--- client/index.js
+++ client/index.js
@@ -1,36 +1,46 @@
-import _ from 'lodash';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './src/styles/styles.css';
-import './src/styles/appStyles.scss';
-import Pi from './src/images/pi.png';
-import Anchor from './src/images/anchor.svg';
+import App from './components/App.jsx';
 
-const component = () => {
-  let element = document.createElement('div');
+ReactDOM.render(<App />, document.getElementById('app'));
 
-  let textElement = document.createElement('div');
-  textElement.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// ReactDOM.render(<App />, document.getElementById('Reviews'));
+
+
+// import _ from 'lodash';
+
+// import './src/styles/styles.css';
+// import './src/styles/appStyles.scss';
+// import Pi from './src/images/pi.png';
+// import Anchor from './src/images/anchor.svg';
+
+// const component = () => {
+//   let element = document.createElement('div');
+
+//   let textElement = document.createElement('div');
+//   textElement.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
-  let pictureElement = document.createElement('div');
-  let pIcon = new Image();
-  pIcon.src = Pi;
-  pictureElement.appendChild(pIcon);
+//   let pictureElement = document.createElement('div');
+//   let pIcon = new Image();
+//   pIcon.src = Pi;
+//   pictureElement.appendChild(pIcon);
 
-  let anchorElement = document.createElement('div');
-  let anchorSvg = new Image();
-  anchorSvg.src = Anchor;
-  anchorElement.classList.add('anchor');
-  anchorElement.appendChild(anchorSvg);
-
-
-  element.appendChild(textElement);
-  element.appendChild(pictureElement);
-  element.appendChild(anchorElement);
-
-  element.classList.add('test');
+//   let anchorElement = document.createElement('div');
+//   let anchorSvg = new Image();
+//   anchorSvg.src = Anchor;
+//   anchorElement.classList.add('anchor');
+//   anchorElement.appendChild(anchorSvg);
 
 
-  return element;
-}
+//   element.appendChild(textElement);
+//   element.appendChild(pictureElement);
+//   element.appendChild(anchorElement);
 
-document.body.appendChild(component());
+//   element.classList.add('test');
+
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
