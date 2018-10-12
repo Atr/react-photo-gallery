7ec155a2ffc26a4cd8f1e524360c6eb709451d30
Alex Romanak <alexromanak@Alexs-MBP.attlocal.net>
2018-10-11 23:17:25 -0700

    Implemented initial carousel slide

--- client/components/PhotoCarousel.jsx
+++ client/components/PhotoCarousel.jsx
@@ -3,19 +3,12 @@ import React from 'react';
 import PhotoCarouselPhoto from './PhotoCarouselPhoto';
 
 const PhotoCarousel = props => {
-  const leftMargin = props.focusPhotoIndex * -75;
-  
-  // This works, but is crude:
-  // style={{marginLeft: leftMargin + 'px'}}
-
-  // Need to refactor the below to accept an input:
-  // style={{transform: 'translateX(-100px)'}}
-
+  const leftShift = (props.focusPhotoIndex * 110) + 50;
   return (
     <div className='photo-carousel fc jc-c'>
       <div className='photo-carousel-container-wrapper fc jc-c'>
         <div className='fc photo-carousel-container'>
-          <ul className='fc photo-carousel-ul'>
+          <ul className='fc photo-carousel-ul' style={{ transform: `translateX(-${leftShift}px)` }}>
             {props.photos.map( (photo, idx) => {
               if (props.focusPhotoIndex === idx) {
                 return <PhotoCarouselPhoto photo={photo} key={`key${photo.caption}-${photo.img}`} focus={'image-in-focus'} />
--- client/components/PhotoCarouselPhoto.jsx
+++ client/components/PhotoCarouselPhoto.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 const PhotoCarouselPhoto = props => {
-  const classList = 'photo-carousel-list-item ' + props.focus;
-  console.log(classList);
+  const classList = `photo-carousel-list-item  ${props.focus}`;
   return (
     <li className={classList}>
       <img src={props.photo.img} alt={props.photo.caption} className='photo-carousel-image' />
