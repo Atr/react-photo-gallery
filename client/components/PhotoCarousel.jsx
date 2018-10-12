f65cc1b5b4e68f2a5eb2101cb7025963ac3ed4e1
Alex Romanak <alexromanak@Alexs-MBP.attlocal.net>
2018-10-12 07:53:58 -0700

    Refined carousel photo slide

--- client/components/PhotoCarousel.jsx
+++ client/components/PhotoCarousel.jsx
@@ -3,12 +3,23 @@ import React from 'react';
 import PhotoCarouselPhoto from './PhotoCarouselPhoto';
 
 const PhotoCarousel = ({ photos, focusPhotoIndex }) => {
-  const leftShift = (focusPhotoIndex * 110) + 50;
+  let leftShift;
+  let leftMargin;
+  if (focusPhotoIndex > 2) {
+    leftShift = (focusPhotoIndex * 110) + 50;
+    leftMargin = 50;
+  } else {
+    leftShift = 0;
+    leftMargin = 0;
+  }
   return (
     <div className='photo-carousel fc jc-c'>
       <div className='photo-carousel-container-wrapper fc jc-c'>
         <div className='fc photo-carousel-container'>
-          <ul className='fc photo-carousel-ul' style={{ transform: `translateX(-${leftShift}px)` }}>
+          <ul
+            className='fc photo-carousel-ul'
+            style={{ transform: `translateX(-${leftShift}px)`, marginLeft: `${leftMargin}%` }}
+          >
             {photos.map((photo, idx) => {
               if (focusPhotoIndex === idx) {
                 return <PhotoCarouselPhoto photo={photo} key={`key${photo.caption}-${photo.img}`} focus='image-in-focus' />;
