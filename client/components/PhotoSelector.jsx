b10d79ffa4806e309ab441687763aef80e6b5f34
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-10-13 16:12:30 -0700

    Styled left/right buttons

--- client/components/PhotoSelector.jsx
+++ client/components/PhotoSelector.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import LeftArrow from '../src/images/arrow-left.svg';
+import RightArrow from '../src/images/arrow-right.svg';
+
 const PhotoSelector = ({ photo, cycleLeft, cycleRight }) => (
   <div className='photo-selector fc fd-c ai-c'>
     <div className='caption fc jc-c'>
@@ -10,14 +13,14 @@ const PhotoSelector = ({ photo, cycleLeft, cycleRight }) => (
       </div>
     </div>
     <div className='center-photo-selector fc ai-c jc-sb'>
-      <button type='button' className='left-selector-button' onClick={cycleLeft}>
-        Left arrow
+      <button type='button' className='left-selector-button ai-s' onClick={cycleLeft}>
+        <img src={LeftArrow} alt='left arrow icon' className='arrow' />
       </button>
       <div className='center-image-container fc jc-c ai-c'>
         <img src={photo.img} alt={photo.caption} className='center-image' />
       </div>
-      <button type='button' className='right-selector-button' onClick={cycleRight}>
-        Right arrow
+      <button type='button' className='right-selector-button ai-s' onClick={cycleRight}>
+        <img src={RightArrow} alt='right arrow icon' className='arrow'/>
       </button>
     </div>
   </div>
